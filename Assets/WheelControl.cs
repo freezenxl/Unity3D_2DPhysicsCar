9ceffb83822598efcaecb67350d809f3a3130c3c
freezenxl <64265868+freezenxl@users.noreply.github.com>
2020-10-14 22:46:52 +0300

    Add files via upload

---
+++ Assets/WheelControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelControl : MonoBehaviour
+{
+    [SerializeField]private  WheelJoint2D rear;
+    [SerializeField]private WheelJoint2D front;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            front.useMotor = true; 
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            front.useMotor = false; 
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rear.useMotor = true; 
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rear.useMotor = false;
+        }
+    }
+}
